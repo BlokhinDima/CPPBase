dd374feff8809e00b63686869684ffb8521ee0c7
Dmitry Blokhin <blokhindmitrijj@gmail.com>
2023-07-11 17:53:25 +0600

    Add Lesson8/Task1 code

---
+++ Lesson8/Task1/Task1/Task1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+class BadLength : public std::exception
+{
+public:
+    const char* what() const override { return "Bad word length!"; }
+};
+
+
+int function(std::string str, int forbidden_length)
+{
+    if (str.length() != forbidden_length)
+    {
+        return str.length();
+    }
+    else
+    {
+        throw BadLength();
+    }
+}
+
+
+int main()
+{
+    try
+    {
+        int forbidden_length{};
+        std::string word{};
+
+        std::cout << "Input forbidden length: ";
+        std::cin >> forbidden_length;
+
+        for (;;)
+        {
+            std::cout << "Input word: ";
+            std::cin >> word;
+            int word_length = function(word, forbidden_length);
+            std::cout << "Word \"" << word << "\" length is " << word_length << std::endl;
+        }
+    }
+    catch (const BadLength& ex)
+    {
+        std::cout << ex.what() << " Goodbuye!" << std::endl;
+    }
+}
