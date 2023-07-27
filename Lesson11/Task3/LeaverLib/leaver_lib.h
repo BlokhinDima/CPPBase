2cf21c064ff0d9e3724b70a5154eb7dd17ee1171
Dmitry Blokhin <blokhindmitrijj@gmail.com>
2023-07-27 19:30:57 +0600

    Update Lesson11/Task3. Add dll export to CMake

--- Lesson11/Task3/LeaverLib/leaver_lib.h
+++ Lesson11/Task3/LeaverLib/leaver_lib.h
@@ -2,14 +2,8 @@
 
 #include <string>
 
-#ifdef LEAVERLIBDYNAMIC_EXPORTS
-#define LEAVERLIB_API __declspec(dllexport)
-#else
-#define LEAVERLIB_API __declspec(dllimport)
-#endif
-
 class Leaver
 {
 public:
-	LEAVERLIB_API void leave(std::string str);
+	void leave(std::string str);
 };
